3a11420166951a1ed1354b19415013e81ddee557
Jim Lucas <jlucas@cmsws.com>
2020-06-13 08:40:07 -0700

    Account name adjustment in the example data scripts

--- examples/preseem_api_service_new.php
+++ examples/preseem_api_service_new.php
@@ -9,7 +9,7 @@ $attachment->cpe_mac = '00:10:0b:6e:4c:ff';
 $attachment->network_prefixes = array('12.12.12.12');
 p($api->api_services_create([
   'id' => 'ServiceLocation_4321',
-  'account' => 'CustomerName_123',
+  'account' => 'CustomerName_1234',
   'package' => '',
   'parent_device_id' => '',
   'up_speed' => 2000,
@@ -17,5 +17,5 @@ p($api->api_services_create([
   'attachments' => array($attachment),
 ]));
 
-
 p($api->_api_list('services'));
+
